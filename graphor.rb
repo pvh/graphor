9a8b1e659567dd5eefab820e2519910ce33cd05e
Peter van Hardenberg <pvh@heroku.com>
2011-09-24 14:33:38 -0700

    post destroy and create query object

--- graphor.rb
+++ graphor.rb
@@ -3,6 +3,10 @@ require 'sinatra'
 require 'sequel'
 require 'json'
 
+Sequel.connect(ENV["DATABASE_URL"])
+
+require './lib/graphor/query'
+
 class Graphor < Sinatra::Base
 
 set :public, File.dirname(__FILE__) + '/public'
@@ -12,19 +16,43 @@ get '/' do
 end
 
 get "/data" do
-  db = Sequel.connect(params["database_url"])
-  x = []; y = []
-  xlabels = [];
-  db.fetch(params["query"]).each do |row|
-    if row[:x].instance_of? Time
-      xlabels << row[:x].to_s
-      x << row[:x].to_i
-    else
-      x << row[:x]
+  grab_the_data(params["database_url"], params["query"])
+end
+
+delete "/query/:id" do
+  q = Query[:alias => params["id"]]
+  q.destroy
+end
+
+get "/query/:id/results" do
+  q = Query[:alias => params["id"]]
+  grab_the_data(q[:database_url], q[:query])
+end
+
+post "/query" do
+  q = Query.create(
+    :alias => (0...8).map{ ('a'..'z').to_a[rand(26)] }.join,
+    :database_url => params["database_url"],
+    :query => params["query"])
+  q[:alias]
+end
+
+helpers do
+  def grab_the_data(database_url, query)
+    db = Sequel.connect(database_url)
+    x = []; y = []
+    xlabels = [];
+    db.fetch(query).each do |row|
+      if row[:x].instance_of? Time
+        xlabels << row[:x].to_s
+        x << row[:x].to_i
+      else
+        x << row[:x]
+      end
+      y << row[:y]
     end
-    y << row[:y]
+    {:x => x, :xlabels => xlabels, :y => y}.to_json
   end
-  {:x => x, :xlabels => xlabels, :y => y}.to_json
 end
 
 end
